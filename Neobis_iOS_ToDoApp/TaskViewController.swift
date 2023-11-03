37375511e98ed7f0e1464c0e291d9bc8e6b900eb
iPakTulane <ipak.tulane@gmail.com>
2023-11-03 18:52:33 +0500

    Configure appearance of placeholders for text field and text view

--- Neobis_iOS_ToDoApp/TaskViewController.swift
+++ Neobis_iOS_ToDoApp/TaskViewController.swift
@@ -41,12 +41,16 @@ class TaskViewController: UIViewController {
         } else {
             print("Error: TaskItem is nil in viewDidLoad.")
         }
+        
+        setupTextView()
+        setupTextField()
+        
     }
     
     @IBAction func saveButtonTapped(_ sender: UIButton) {
         
-        let titleData = titleTextField.text ?? ""
-        let descriptionData = descriptionTextView.text ?? ""
+        let titleData = titleTextField.text ?? "Title"
+        let descriptionData = descriptionTextView.text ?? "Description"
 
         let taskItem = TaskItem(title: titleData, description: descriptionData, isDone: false, positionOnList: 0)
         
@@ -63,6 +67,49 @@ class TaskViewController: UIViewController {
     
     @IBAction func discardButtonTapped(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension TaskViewController: UITextViewDelegate {
+    
+    func setupTextView() {
+        // Set the placeholder text for descriptionTextView
+        descriptionTextView.text = "Description"
+        descriptionTextView.textColor = UIColor.lightGray
+        descriptionTextView.font = .systemFont(ofSize: 15)
+        // Set the delegate for descriptionTextView
+        descriptionTextView.delegate = self
+        
+        // Customize the appearance of the descriptionTextView
+        descriptionTextView.layer.borderWidth = 1.0 // Border width
+        descriptionTextView.layer.borderColor = UIColor.lightGray.cgColor // Border color
+        descriptionTextView.layer.cornerRadius = 5.0 // Corner radius (for rounded corners)
+        descriptionTextView.clipsToBounds = true // Ensure the border doesn't exceed the bounds
+    }
+    
+    func setupTextField() {
+        // Set the placeholder text for titleTextField
+        titleTextField.text = "Title"
+        titleTextField.textColor = UIColor.lightGray
+        titleTextField.font = .systemFont(ofSize: 15)
+        
+        // Customize the appearance of the descriptionTextView
+        titleTextField.layer.borderWidth = 1.0 // Border width
+        titleTextField.layer.borderColor = UIColor.lightGray.cgColor // Border color
+        titleTextField.layer.cornerRadius = 5.0 // Corner radius (for rounded corners)
+        titleTextField.clipsToBounds = true // Ensure the border doesn't exceed the bounds
+        
+    }
+    
+    
+    
+    // UITextViewDelegate method to clear placeholder text when editing starts
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == "Description" {
+            textView.text = ""
+            textView.textColor = UIColor.black // Set the text color to black or your preferred color
+        }
     }
     
 }
